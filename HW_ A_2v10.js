7ece2abd212dd258d1904f2f046c19f2b5444bee
LiliaVin <salinx@mail.ru>
2022-04-28 18:41:48 +0400

    stepen

---
+++ HW_ A_2v10.js
@@ -0,0 +1,35 @@
+// let i = 1;
+// let result = 1;
+// while( i < 11) {
+//     result = result *2
+//     i++
+// } console.log(result)
+
+// const base1 = function(num) {
+//     return 2**num
+// } 
+// console.log(base1(10))
+
+
+
+const base2 = function(num) {
+    return Math.pow(2,num)
+} 
+console.log(base2(10))
+
+
+// 1. Написать скриптик, который сосчитает и выведет результат от возведения 2 в степень 10,
+//  начиная со степени 1
+
+
+// let base = 2;
+// let exp = 10;
+// let result = 1;
+// let count = 1;
+// while (count <= exp) {
+//     result = result*base;
+//     console.log(result)
+
+//   count ++;
+// }
+// ;
