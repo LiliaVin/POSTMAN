8bf858b098c21862b5ed016bda76ec1e6f2089fc
LiliaVin <salinx@mail.ru>
2022-04-28 10:00:33 +0400

    Function with "Age"

---
+++ Anatoly1.js
@@ -0,0 +1,19 @@
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
+
+const checkAge = function(age){
+
+if (age < age_2) {
+    console.log ( " You dont have access cause your age is "+ age + ". Its less then " + age_2)
+} else if (age >= age_2 && age < age_3){
+    console.log("Welcome!")
+} else if ( age > age_3){
+    console.log( "Keep calm! and go sleep")
+} else {
+    console.log("Technical work")
+}
+}
+
+checkAge(17)
+checkAge(19)
