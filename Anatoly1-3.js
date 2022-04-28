e9e4e282f1915b3301df8ee6325eff8dba8add85
LiliaVin <salinx@mail.ru>
2022-04-28 10:31:40 +0400

    isNan

---
+++ Anatoly1-3.js
@@ -0,0 +1,22 @@
+const age_1 = 10;
+const age_2 = 18;
+const age_3 = 60;
+
+const checkAge = function(age){
+    age = Number(age)
+    if (!isNaN(age)) {
+
+    if (age < age_2) {
+    console.log ( "You dont have access cause your age is "+ age + ". Its less then " + age_2)
+}   else if (age >= age_2 && age < age_3){
+    console.log("Welcome!")
+}   else if ( age > age_3){
+    console.log( "Keep calm! and go sleep")
+}   else {
+    console.log("Technical work")
+} 
+}   else {console.log ("Not integer")}
+}
+checkAge(17)
+checkAge("22ddddd")
+checkAge("34")
