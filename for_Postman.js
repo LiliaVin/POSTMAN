c753e27e18190b680c1b2f9a0830b32c3656fafa
LiliaVin <salinx@mail.ru>
2022-05-04 09:50:08 +0400

    Strelochnaya

--- for_Postman.js
+++ for_Postman.js
@@ -1,7 +1,29 @@
-function yolka(item_1, item_2) {
-    let mass = "Hello!!!"
-    console.log(mass, item_1,item_2)
-    console.log(item_1+item_2)
+// function yolka(item_1, item_2) {
+//     let mass = "Hello!!!"
+//     console.log(mass, item_1,item_2)
+//     console.log(item_1+item_2)
 
-}
-yolka(11 ,44)
+// }
+// yolka(11 ,44)
+// function f11() {
+//     console.log("Hi")
+// }
+
+let t1 = 10
+let t2 = 20
+
+let t3 = 30
+let t4 = 40
+
+let f1 = () => {console.log("Hello!!!!!!!!!!!!!")}
+
+let f22 = (t11,t22) => {console.log("Sum = " , t11 + t22)}
+
+let f33 = (t1 > t2) ?
+(tt1,tt2) => console.log("order = ", tt1 + tt2):
+(tt1,tt2) => console.log("order = ", tt1 - tt2);
+
+f33 (t3,t4)
+
+// f11()
+// f22(t1, t2)
